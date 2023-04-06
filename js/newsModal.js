d02a19404f3b8147d012fa943d6f80350d270ab8
TuqalQasim <qasimovtural572@gmail.com>
2023-04-06 22:15:07 +0400

    commit

---
+++ js/newsModal.js
@@ -0,0 +1,27 @@
+const vacansyModal = document.querySelector(".vacancy_modal");
+const vacancyModalBg = document.querySelector(".vacansy_modal_bg");
+const vacancyOverlay = document.querySelector(".vacancy_overlay");
+const vacamcyClose = document.querySelector(".fa-solid");
+const newsItem = document.querySelectorAll(".news_item");
+const checkbox = document.querySelector(".checkbox");
+const overlay = document.querySelector(".burger_overlay");
+overlay.addEventListener("click", () => {
+  checkbox.checked = false;
+});
+newsItem.forEach((a) => {
+  a.addEventListener("click", () => {
+    vacancyModalBg.style.visibility = "visible";
+    vacancyModalBg.style.opacity = "1";
+    vacansyModal.style.transform = "scale(1)";
+  });
+});
+vacancyOverlay.addEventListener("click", () => {
+  vacancyModalBg.style.visibility = "hidden";
+  vacancyModalBg.style.opacity = "0";
+  vacansyModal.style.transform = "scale(0.8)";
+});
+vacamcyClose.addEventListener("click", () => {
+  vacancyModalBg.style.visibility = "hidden";
+  vacancyModalBg.style.opacity = "0";
+  vacansyModal.style.transform = "scale(0.8)";
+});
